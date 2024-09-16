e880f98e78593431926ae2fa2b287c491fb3f501
Haibara-69 <1007902128@qq.com>
2024-09-16 18:54:08 -0400

    Getting started with Kanbas A1

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,12 +7,30 @@ export default function Labs() {
   return (
     <div id="wd-labs">
       <h1>Labs</h1>
+
+      {/* full name */}
+      <h2>Yiqun Cao 01</h2>
+
       <TOC />
+
+      {/* GitHub link */}
+      <div>
+        <a
+          id="wd-github"
+          href="https://github.com/Haibara-69/kanbas-react-web-app-cs5610-fa24/tree/a1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View on GitHub
+        </a>
+      </div>
+
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
+
       </Routes>
     </div>
 );}
